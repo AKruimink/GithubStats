c2d8d612c1c0272b68ebe529a3902cea08c3cd2c
AKruimink <AlwinKruimink@gmail.com>
2020-09-12 17:13:45 +0100

    Add event handler to the get stats button

--- src/js/main.js
+++ src/js/main.js
@@ -6,21 +6,26 @@ class Main {
      * Intializes a new instance of Main
      */
     constructor() {
-        var _a, _b;
+        var _a, _b, _c;
         /**
          * Root address to the github api
          */
         this.apiRoot = "https://api.github.com/";
-        this.validateInput();
-        (_a = document.getElementById("username")) === null || _a === void 0 ? void 0 : _a.addEventListener("keyup", this.validateInput);
-        (_b = document.getElementById("repository")) === null || _b === void 0 ? void 0 : _b.addEventListener("keyup", this.validateInput);
-        ;
+        this.validateUserInput();
+        // Setup form event handlers
+        (_a = document.getElementById("username")) === null || _a === void 0 ? void 0 : _a.addEventListener("keyup", this.validateUserInput);
+        (_b = document.getElementById("repository")) === null || _b === void 0 ? void 0 : _b.addEventListener("keyup", this.validateUserInput);
+        // Setup search event handler
+        (_c = document.getElementById("get-stats-button")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", event => this.onGetStatsButtonClicked(event));
+        // Setup page view
+        let username = this.getQueryVariable("username");
+        let repository = this.getQueryVariable("repository");
     }
     /**
      * Validates the user input
      * If the user input is valid the get-stats-button will be enabled, if not it will be disabled
      */
-    validateInput() {
+    validateUserInput() {
         var _a, _b;
         let username = (_a = document.getElementById("username")) === null || _a === void 0 ? void 0 : _a.value;
         let repository = (_b = document.getElementById("repository")) === null || _b === void 0 ? void 0 : _b.value;
@@ -37,6 +42,61 @@ class Main {
             else {
                 getStatsButton.disabled = true;
             }
+        }
+    }
+    /**
+     * Gets the value of a query variable
+     * @param variableName Name of the query variable to get
+     */
+    getQueryVariable(variableName) {
+        let query = window.location.search.substring(1);
+        let variables = query.split("&");
+        for (var i = 0; i < variables.length; i++) {
+            let pair = variables[i].split("=");
+            if (pair[0] == variableName) {
+                return pair[1];
+            }
+        }
+        return "";
+    }
+    /**
+     * Sets a new query variable
+     * @param variableName Name of the variable to set
+     * @param variableValue Value to assign to the new variable
+     */
+    setQueryVariable(variableNames, variableValues) {
+        if (variableNames.length != variableValues.length) {
+            return;
+        }
+        let query = window.location.search.substring(1);
+        for (let i = 0; i < variableNames.length; i++) {
+            if (query.length <= 0) {
+                query = query.concat("?");
+            }
+            else {
+                query = query.concat("&");
+            }
+            query = query.concat(variableNames[i] + "=" + variableValues[i]);
+        }
+        window.location.href = query;
+        // if(query.length <= 0) {
+        //     query = query.concat("?");
+        // }
+        // else {
+        //     query = query.concat("&");
+        // }
+        // query = query.concat(variableName + "=" + variableValue);
+        // window.location.href = query;
+    }
+    /**
+     * Invoked when the Get Stats Button is clicked and  gets all information of a repository
+     */
+    onGetStatsButtonClicked(eventArgs) {
+        var _a, _b;
+        let username = (_a = document.getElementById("username")) === null || _a === void 0 ? void 0 : _a.value;
+        let repository = (_b = document.getElementById("repository")) === null || _b === void 0 ? void 0 : _b.value;
+        if (username != null && repository != null) {
+            this.setQueryVariable(["username", "repository"], [username, repository]);
         }
     }
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -11,20 +11,28 @@ class Main {
      * Intializes a new instance of Main
      */
     constructor() {
-        this.validateInput();
+        this.validateUserInput();
 
-        (document.getElementById("username") as HTMLInputElement)?.addEventListener("keyup", this.validateInput);
-        (document.getElementById("repository") as HTMLInputElement)?.addEventListener("keyup", this.validateInput);;
+        // Setup form event handlers
+        (document.getElementById("username") as HTMLInputElement)?.addEventListener("keyup", this.validateUserInput);
+        (document.getElementById("repository") as HTMLInputElement)?.addEventListener("keyup", this.validateUserInput);
+
+        // Setup search event handler
+        (document.getElementById("get-stats-button") as HTMLButtonElement)?.addEventListener("click", event => this.onGetStatsButtonClicked(event));
+
+        // Setup page view
+        let username: string = this.getQueryVariable("username");
+        let repository: string = this.getQueryVariable("repository");
     }
 
     /**
      * Validates the user input
      * If the user input is valid the get-stats-button will be enabled, if not it will be disabled
      */
-    public validateInput(): void {
-        let username = (document.getElementById("username") as HTMLInputElement)?.value;
-        let repository = (document.getElementById("repository") as HTMLInputElement)?.value;
-        let getStatsButton = (document.getElementById("get-stats-button") as HTMLButtonElement);
+    public validateUserInput(): void {
+        let username: string = (document.getElementById("username") as HTMLInputElement)?.value;
+        let repository: string = (document.getElementById("repository") as HTMLInputElement)?.value;
+        let getStatsButton: HTMLButtonElement = (document.getElementById("get-stats-button") as HTMLButtonElement);
 
         if(getStatsButton != null)
         {
@@ -39,6 +47,61 @@ class Main {
             else {
                 getStatsButton.disabled = true;
             }
+        }
+    }
+
+    /**
+     * Gets the value of a query variable
+     * @param variableName Name of the query variable to get
+     */
+    private getQueryVariable(variableName: string): string{
+        let query: string = window.location.search.substring(1);
+        let variables: string[] = query.split("&");
+
+        for(var i = 0; i < variables.length; i++) {
+            let pair: string[] = variables[i].split("=");
+
+            if(pair[0] == variableName) {
+                return pair[1];
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Sets a new query variable
+     * @param variableName Name of the variable to set
+     * @param variableValue Value to assign to the new variable
+     */
+    private setQueryVariable(variableNames: string[], variableValues: string[]): void {
+        if(variableNames.length != variableValues.length) {
+            return;
+        }
+        
+        let query: string = window.location.search.substring(1);
+
+        for(let i = 0; i < variableNames.length; i++) {
+            if(query.length <= 0) {
+                query = query.concat("?");
+            }
+            else {
+                query = query.concat("&");
+            }
+
+            query = query.concat(variableNames[i] + "=" + variableValues[i]);
+        }
+        window.location.href = query;
+    }
+
+    /**
+     * Invoked when the Get Stats Button is clicked and  gets all information of a repository
+     */
+    private onGetStatsButtonClicked(eventArgs: Event): void{
+        let username: string = (document.getElementById("username") as HTMLInputElement)?.value;
+        let repository: string = (document.getElementById("repository") as HTMLInputElement)?.value;
+
+        if(username != null && repository != null) {
+            this.setQueryVariable(["username", "repository"],[username, repository]);
         }
     }
 }
